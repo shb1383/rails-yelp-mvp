dbc601d29b8ce2c26a83bd2321afdf6e2da2f4bc
syrene bartolome <syrene.h.bartolome@icloud.com>
2024-01-11 20:56:52 +0900

    finished basic views for restaurants and reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:rating).permit(:content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'destroying old restaurants...'
+Restaurant.destroy_all
+
+puts 'creating 5 restaurants...'
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+5.times do
+  resto = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category.sample
+  )
+  puts "#{resto.name} has been created."
+end
+puts 'Done!'
